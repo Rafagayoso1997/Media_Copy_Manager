e8b5cb4572a6a76312a382c86280c5b2071c01b1
Rafael <gayoso0597@gmail.com>
2021-02-18 15:32:58 -0500

    Se peuden añadir categorias

--- ModernUINavigationApp1/gui/Pages/nomencladores/PCategorias.xaml.cs
+++ ModernUINavigationApp1/gui/Pages/nomencladores/PCategorias.xaml.cs
@@ -156,7 +156,7 @@ namespace MCP.gui.Pages.nomencladores
             refreshGrid();
             clearForm();
             hideForm();
-            entity = null;
+            entity = new categoria();
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
